6c14cc951083ae99e07875c4e96477e49084240c
Danila <da_buddha@bk.ru>
2022-05-26 00:49:36 +0300

    Небольшие изменения

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,39 @@
-btnOpenModal = document.querySelector('#openModal')
-modal = document.querySelector('#modal')
-modalTrigger = document.querySelector('#modal__trigger')
+const secondForStart = 6
+document.addEventListener("DOMContentLoaded", startLeadCatcher(secondForStart));
 
 
-let onceOpen = false
-let timerBoolean = false 
-
-const closeModal = () => {
-    modal.classList.toggle('show')
-    document.body.classList.remove('overflow-hidden')
+function startLeadCatcher (secondForStart)  {
+    btnOpenModal = document.querySelector('#openModal')
+    modal = document.querySelector('#modal')
+    modalTrigger = document.querySelector('#modal__trigger')
+    btnModalClose = document.querySelector('#btnModalClose')
+    
+    let onceOpen = false
+    let timerBoolean = false 
+    
+    const closeModal = () => {
+        modal.classList.toggle('show')
+        document.body.classList.remove('overflow-hidden')
+    }
+    const openModal = () => {
+        modal.classList.toggle('show')
+        document.body.classList.add('overflow-hidden')
+    }
+    const triggerModal = () => {
+        if (!onceOpen && timerBoolean) {
+            openModal()
+            onceOpen=true
+        } 
+    }
+    
+    setTimeout(()=>timerBoolean=true,secondForStart+'000')
+    
+    
+    btnOpenModal.addEventListener('click',openModal)
+    modal.addEventListener('click',closeModal)
+    btnModalClose.addEventListener('click',closeModal)
+    modalTrigger.addEventListener('mouseout',triggerModal);
+    
+    
+    modalWindow.addEventListener('click',e=>e.stopPropagation())
 }
-const openModal = () => {
-    modal.classList.toggle('show')
-    document.body.classList.add('overflow-hidden')
-}
-const triggerModal = () => {
-    if (!onceOpen && timerBoolean) {
-        openModal()
-        onceOpen=true
-    } 
-}
-
-setTimeout(()=>timerBoolean=true,4000)
-
-
-btnOpenModal.addEventListener('click',openModal)
-modal.addEventListener('click',closeModal)
-modalTrigger.addEventListener('mouseout',triggerModal);
-
-
-modalWindow.addEventListener('click',e=>e.stopPropagation())
